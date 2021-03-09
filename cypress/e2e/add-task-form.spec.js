f7674153274ce712aac057d515b606f0d230ea3e
alicanerdurmaz <alicanerdurmaz@gmail.com>
2021-03-09 19:21:07 +0300

    test: task form

---
+++ cypress/e2e/add-task-form.spec.js
@@ -0,0 +1,32 @@
+import { TASKS_API_BASE_URL } from '../../src/constans';
+
+describe('Add Task Form', () => {
+  it('Add Task Form works properly', () => {
+    const TASK_TITLE = 'Test Task';
+    const MOCK_TASK = { id: '999', title: TASK_TITLE, completed: false };
+
+    cy.intercept('GET', TASKS_API_BASE_URL, [])
+      .as('getTasks')
+      .intercept('POST', TASKS_API_BASE_URL, {
+        body: MOCK_TASK,
+        delayMs: 1000,
+      })
+      .as('addTask')
+      .as('deleteTask')
+      .visit('/')
+      .wait('@getTasks')
+      .get('.form-control')
+      .type(TASK_TITLE)
+      .get(':nth-child(2) > .btn')
+      .click()
+      .get('.form-control')
+      .should('be.disabled')
+      .get(':nth-child(2) > .btn')
+      .should('be.disabled')
+      .wait('@addTask')
+      .get('.form-control')
+      .should('be.not.disabled')
+      .get(':nth-child(2) > .btn')
+      .should('be.not.disabled');
+  });
+});
